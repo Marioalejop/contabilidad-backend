1ea854aff96e2415942e0766af66314b47fdf7b1
marioalejop <marioalejop@gmail.com>
2026-09-18 20:16:06 -0500

    Configura conexión a base de datos y estructura de paquetes

---
+++ src/main/java/com/contabilidad/contabilidad_backend/ContabilidadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.contabilidad.contabilidad_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilidadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilidadBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contabilidad/contabilidad_backend/ContabilidadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contabilidad.contabilidad_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilidadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
